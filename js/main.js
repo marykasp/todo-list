fe402ddaa54cbe1ff24fb60d082a81ea917dd8c9
Mary Kasparian <kasparian.mary@gmail.com>
2022-05-11 13:42:45 -0500

    Code display todos function, create elements for each new todo item, append to todo list

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ window.addEventListener('load', () => {
     localStorage.setItem('username', e.target.value);
   });
 
+  // event listener - form submission
   newTodoForm.addEventListener("submit", e => {
     e.preventDefault();
     // console.log(e.target.elements)
@@ -27,17 +28,79 @@ window.addEventListener('load', () => {
       createAt: new Date().getTime()
     }
 
-    // add todo to global todos array
-    todos.push(todo)
+    // check if user entered a content and category name - if not show an error message
 
-    // turn todos list into a JSON string to store in local storage
-    localStorage.setItem('todos', JSON.stringify(todos))
+    // add todo to global todos array
+    todos.push(todo);
+
+    // turn global todos list into a JSON string to store in local storage
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    // reset form
+    e.target.reset()
+
+    // Display todos to app
+    displayTodos();
   })
-  // div of todolist items
+})
+
+// FUNCTIONS
+
+const displayTodos = () => {
+  // select todo list div that contains the todo items
   const todoList = document.querySelector('#todo-list');
 
+  todoList.innerHTML = '';
+
+  // have access to global todos array variable where the todo items are stored
+  todos.forEach(todo => {
+    // create an element for each todo
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo-item');
+
+    // label with input and span
+    const label = document.createElement('label');
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+
+    const content = document.createElement('div');
+    const actions = document.createElement('div');
+    const edit = document.createElement('button');
+    const deleteButton = document.createElement('button')
 
 
+    input.type = 'checkbox';
+    input.checked = todo.done;
+    span.classList.add('bubble');
+    // change classList of span based on category
+    if(todo.category === 'personal') {
+      span.classList.add('personal')
+    } else {
+      span.classList.add('work')
+    }
+    // append input and span to label element - children
+    label.appendChild(input);
+    label.appendChild(span)
 
-  // display todos
-})
+    content.classList.add('todo-content');
+    actions.classList.add('todo-actions');
+    edit.classList.add('edit');
+    deleteButton.classList.add('delete');
+
+    content.innerHTML =  `<input type="text" value=${todo.content} readonly>`
+    edit.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+
+    // append edit and delete buttons to actions div
+    actions.appendChild(edit);
+    actions.appendChild(deleteButton);
+
+    // append label, content, and actions to todoItem
+    todoItem.appendChild(label);
+    todoItem.appendChild(content);
+    todoItem.appendChild(actions);
+
+    // append new todoItem to todoList
+    todoList.appendChild(todoItem)
+  })
+}
