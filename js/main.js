8992af0f0fd8ba9b2bda725ddce4d5419308f5af
Mary Kasparian <kasparian.mary@gmail.com>
2022-05-11 13:22:44 -0500

    Add event listener to form submission, create new todo object and push to todos array

--- js/main.js
+++ js/main.js
@@ -13,6 +13,25 @@ window.addEventListener('load', () => {
   // event listener on name input - store input value to localStorage
   nameInput.addEventListener("change", (e) => {
     localStorage.setItem('username', e.target.value);
+  });
+
+  newTodoForm.addEventListener("submit", e => {
+    e.preventDefault();
+    // console.log(e.target.elements)
+    const todo = {
+      // input with name content, get the value
+      content: e.target.elements.content.value,
+      // input with name category
+      category: e.target.elements.category.value,
+      done: false,
+      createAt: new Date().getTime()
+    }
+
+    // add todo to global todos array
+    todos.push(todo)
+
+    // turn todos list into a JSON string to store in local storage
+    localStorage.setItem('todos', JSON.stringify(todos))
   })
   // div of todolist items
   const todoList = document.querySelector('#todo-list');
